d2a77441dd4dcb574aa1b0a3db89ba3905c5a860
tomr-ninja <tomr.ninja@proton.me>
2025-01-20 23:51:19 +0100

    better cmd

--- cmd/geoip/main.go
+++ cmd/geoip/main.go
@@ -1,56 +1,116 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"os"
+
+	"github.com/tomr-ninja/flag3"
 
 	"github.com/tomr-ninja/birdwatcher"
 )
 
 const (
-	defaultNetFilePath = "data/GeoLite2-City-Blocks-IPv4.csv"
-	defaultLocFilePath = "data/GeoLite2-City-Locations-en.csv"
+	defaultNetFilePath  = "data/GeoLite2-City-Blocks-IPv4.csv"
+	defaultLocFilePath  = "data/GeoLite2-City-Locations-en.csv"
+	defaultDumpFilePath = "data/geoip.dat"
 )
 
-var myIP = net.ParseIP("5.28.100.233").To4()
-
 func main() {
-	//loadCSV(true)
-	loadDump()
-}
+	tree := flag3.NewCLI()
+	tree.Subcommand("compile")
+	tree.Subcommand("exec")
+	tree.Subcommand("run")
 
-func loadDump() {
-	ipDB, err := birdwatcher.LoadFromDump("data/geoip.dat")
+	cmd, err := flag3.ParseCLI(tree)
 	if err != nil {
-		panic(err)
+		fatal(err)
 	}
 
-	geo, err := ipDB.LookupIP(myIP)
-	if err != nil {
-		fmt.Println(err)
-		return
+	_ = cmd.Next()
+	executableName := cmd.Command()
+
+	ok := cmd.Next()
+	if !ok {
+		fmt.Printf("Usage: %s [compile|exec|run]\n", executableName)
 	}
 
-	fmt.Printf("IP=%s found: %+v", myIP, geo)
-}
+	switch cmd.Command() {
+	case "compile":
+		var blocksCSVPath, locationsCSVPath, dumpPath string
 
-func loadCSV(dump bool) {
-	ipDB, err := birdwatcher.LoadFromCSV(defaultNetFilePath, defaultLocFilePath)
-	if err != nil {
-		panic(err)
-	}
+		fs := flag.NewFlagSet("compile", flag.ContinueOnError)
+		fs.StringVar(&blocksCSVPath, "blocks-csv", defaultNetFilePath, "Path to GeoLite2-City-Blocks-IPv4.csv")
+		fs.StringVar(&locationsCSVPath, "locations-csv", defaultLocFilePath, "Path to GeoLite2-City-Locations-{locale}.csv")
+		fs.StringVar(&dumpPath, "out", defaultDumpFilePath, "Compiled database output path")
 
-	geo, err := ipDB.LookupIP(myIP)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Printf("IP=%s found: %+v", myIP, geo)
-
-	if dump {
-		if err = birdwatcher.Dump(ipDB, "data/geoip.dat"); err != nil {
-			panic(err)
+		if err = fs.Parse(cmd.Args()); err != nil {
+			fatal(err)
 		}
+
+		if err = compile(blocksCSVPath, locationsCSVPath, dumpPath); err != nil {
+			fatal(err)
+		}
+	case "exec":
+		var dumpPath, ipArg string
+
+		fs := flag.NewFlagSet("exec", flag.ContinueOnError)
+		fs.StringVar(&dumpPath, "db", defaultDumpFilePath, "Path to database file")
+
+		if err = fs.Parse(cmd.Args()); err != nil {
+			fatal(err)
+		}
+
+		if len(fs.Args()) < 1 {
+			fatal(fmt.Errorf("missing IP argument"))
+		}
+		ipArg = fs.Args()[0]
+
+		if err = exec(dumpPath, ipArg); err != nil {
+			fatal(err)
+		}
+
+	case "run":
+		panic("not implemented")
+	}
+}
+
+func exec(dumpPath, ipArg string) error {
+	ipDB, err := birdwatcher.LoadFromDump(dumpPath)
+	if err != nil {
+		return err
+	}
+
+	geo, err := ipDB.LookupIP(net.ParseIP(ipArg).To4())
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf(
+		"IP=%s found:\n  - City: %s\n  - Region: %s\n  - Country: %s (%s)\n",
+		ipArg, geo.City, geo.Region, geo.Country, string(geo.Country[:]),
+	)
+
+	return nil
+}
+
+func compile(netFilePath, locFilePath, outPath string) error {
+	ipDB, err := birdwatcher.LoadFromCSV(netFilePath, locFilePath)
+	if err != nil {
+		return err
+	}
+
+	if err = birdwatcher.Dump(ipDB, outPath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fatal(err error) {
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,12 @@
 module github.com/tomr-ninja/birdwatcher
 
-go 1.22.4
+go 1.23
+
+toolchain go1.23.1
 
 require github.com/tinylib/msgp v1.2.5
 
-require github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
+require (
+	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
+	github.com/tomr-ninja/flag3 v0.1.0 // indirect
+)
