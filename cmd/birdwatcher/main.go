2123ff2572150a232a27eb5b71c302433086e9ab
tomr-ninja <tomr.ninja@proton.me>
2025-01-30 08:30:33 +0100

    add flags

--- cmd/birdwatcher/main.go
+++ cmd/birdwatcher/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
 	"net/http"
@@ -8,15 +10,29 @@ import (
 	"github.com/tomr-ninja/birdwatcher"
 )
 
+const (
+	defaultDumpFilePath = "data/geoip.dat"
+	defaultListenPort   = 8080
+)
+
 func main() {
-	ipdb, err := birdwatcher.LoadFromDump("data/geoip.dat")
+	var (
+		dumpPath string
+		port     uint64
+	)
+
+	flag.StringVar(&dumpPath, "db", defaultDumpFilePath, "Path to GeoIP database file")
+	flag.Uint64Var(&port, "port", defaultListenPort, "Port to listen on")
+	flag.Parse()
+
+	ipdb, err := birdwatcher.LoadFromDump(dumpPath)
 	if err != nil {
 		log.Fatal("failed to load IP database:", err)
 	}
 
 	http.Handle("/parse", &birdwatcher.UserHandler{IPDB: ipdb})
 
-	if err = http.ListenAndServe(":8080", nil); err != nil {
+	if err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		log.Fatal("failed to start server:", err)
 	}
 }
