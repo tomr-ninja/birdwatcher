9a5e3ac8167e906ddf7f006b90cf71280cbc13f0
tomr-ninja <tomr.ninja@proton.me>
2025-04-10 13:11:49 +0200

    improve test

--- server_test.go
+++ server_test.go
@@ -18,12 +18,13 @@ func TestPixelHandler_ServeHTTP(t *testing.T) {
 	req, _ := http.NewRequest(http.MethodGet, "https://example.org/pixel.gif", nil)
 	req.Header.Set("Referer", "https://example.org/some/page")
 	req.Header.Set("User-Agent", `Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.0.0 Mobile Safari/537.36`)
-	pixelResp := httptest.NewRecorder()
 
-	h.ServeHTTP(pixelResp, req)
-
-	if pixelResp.Code != http.StatusOK {
-		t.Errorf("expected status %d, got %d", http.StatusOK, pixelResp.Code)
+	for i := 0; i < 3; i++ {
+		pixelResp := httptest.NewRecorder()
+		h.ServeHTTP(pixelResp, req)
+		if pixelResp.Code != http.StatusOK {
+			t.Errorf("expected status %d, got %d", http.StatusOK, pixelResp.Code)
+		}
 	}
 
 	metricsResp := httptest.NewRecorder()
@@ -34,7 +35,7 @@ func TestPixelHandler_ServeHTTP(t *testing.T) {
 	}
 
 	actual := metricsResp.Body.String()
-	expected := `page_visits{host="example.org",page="/some/page",country="DE",region="Land Berlin",city="Berlin",is_eu="true",ua_name="Chrome",ua_version="93",os="Android",is_bot="false",is_mobile="true"} 1` + "\n"
+	expected := `page_visits{host="example.org",page="/some/page",country="DE",region="Land Berlin",city="Berlin",is_eu="true",ua_name="Chrome",ua_version="93",os="Android",is_bot="false",is_mobile="true"} 3` + "\n"
 	if actual != expected {
 		t.Errorf("metrics: expected %q, got %q", expected, actual)
 	}
